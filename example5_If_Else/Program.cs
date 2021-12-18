2f9c24bd00451046317aea6d0483c89a9403935f
Mikhail Gorbatyuk <MikeGor2001@mail.ru>
2021-12-18 23:54:05 +0300

    add if_else

---
+++ example5_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "миша")
+{
+    Console.WriteLine("Ура, Миша ты вернулся!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
