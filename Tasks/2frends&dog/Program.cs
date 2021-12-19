112a525796690799a0a89b2ba139535c6f5d9f45
Mikhail Gorbatyuk <MikeGor2001@mail.ru>
2021-12-19 23:05:11 +0300

    Add task 2frends&dog not verified

---
+++ Tasks/2frends&dog/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите расстояние между друзьями - ");
+double distance = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите скорость первого мальчика - ");
+double speed_1_boy = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите скорость второго мальчика - ");
+double speed_2_boy = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите скорость собаки - ");
+double speed_dog = Convert.ToDouble(Console.ReadLine());
+int N = 0;
+int i = 2;
+double time = 0;
+if (distance > 2)
+{
+    if (i == 1)
+    {
+        time = distance/(speed_1_boy + speed_dog);
+        i = 2;
+    }
+    else
+    {
+        time = distance/(speed_2_boy + speed_dog);
+        i = 1;
+    }
+    distance = distance - (time*(speed_1_boy + speed_2_boy));
+    N++;
+}
+Console.Write("Собака успеет сбегать между друзьями - " + N + " раз.");
