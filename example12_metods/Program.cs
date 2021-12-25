5ee940feab067805cd5b0b00cf741147a4eb5150
Mikhail Gorbatyuk <MikeGor2001@mail.ru>
2021-12-25 22:36:58 +0300

    Разновидности методов

---
+++ example12_metods/Program.cs
@@ -0,0 +1,49 @@
+﻿//вид 1
+void Method1()
+{
+    Console.WriteLine("Автор MiG @");
+}
+//Method1();
+
+//вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+//Method2(msg: "какой то текст");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21(msg: "ТЕКСТ", count: 6);
+//Method21(count: 6, msg: "НОВЫЙ ТЕКСТ");
+
+//вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+//int year = Method3();
+//Console.WriteLine(year);
+
+//вид 4
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; // String.Empty - пустая строка
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(4, "qwerty");
+Console.WriteLine(res);
